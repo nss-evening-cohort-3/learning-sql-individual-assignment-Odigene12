c089d0ab97ce638df56b69378dae6b68b7068415
Odigene Joseph <Odigene12@gmail.com>
2016-08-31 17:33:22 -0500

    Completed Exercise 9

---
+++ Queries/9.sql
@@ -0,0 +1,2 @@
+SELECT Count (InvoiceDate) As '2009 Invoice Total' From Invoice Where InvoiceDate like  "2009%"
+SELECT Count (InvoiceDate) As '2011 Invoice Total' From Invoice Where InvoiceDate like  "2011%"
