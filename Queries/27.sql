377161a6a02fa266111c87368608a5995331372d
Odigene Joseph <Odigene12@gmail.com>
2016-09-01 19:20:45 -0500

    Completed Exercise 27 and modified Exercise 26 to just show Artist and Total Sales

--- Queries/27.sql
+++ Queries/27.sql
@@ -1,8 +1,9 @@
-SELECT  Artist.Name As "Artist", Total  FROM Album
+SELECT MediaType.Name As "Media Type", Total  FROM Album
 Join Artist On Artist.ArtistId == Album.ArtistId
 Join Track On Track.AlbumId == Album.AlbumId
 Join InvoiceLine On Track.TrackId == InvoiceLine.TrackId
+Join MediaType On MediaType.MediaTypeId == Track.MediaTypeId
 Join Invoice On InvoiceLine.InvoiceId == Invoice.InvoiceId
-Group By Track.Name
 Order By Total DESC
-Limit 3
+Limit 1
+
